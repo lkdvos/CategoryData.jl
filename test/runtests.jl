8f1af6d9698e9cc9af4ffd5a5e91e29e0e70d051
Boris De Vos <boris.devos@ugent.be>
2025-01-30 12:46:43 +0100

    replace TensorKit -> TensorKitSectors in tests as well

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using CategoryData
 using Test, TestExtras, Random
-using TensorKit
-using TensorKit: fusiontensor, pentagon_equation, hexagon_equation
+using TensorKitSectors
+using TensorKitSectors: fusiontensor, pentagon_equation, hexagon_equation
 using Base.Iterators: product
 using LinearAlgebra: LinearAlgebra
 

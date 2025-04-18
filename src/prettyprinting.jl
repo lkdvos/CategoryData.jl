4e35e3a053238be35f1831b7db31ea10d067fa2c
Boris De Vos <boris.devos@ugent.be>
2025-04-18 15:48:49 +0200

    add check to `@objectnames` on number of objects

--- src/prettyprinting.jl
+++ src/prettyprinting.jl
@@ -31,6 +31,7 @@ macro objectnames(categoryname, names...)
     if Meta.isexpr(categoryname, :(=), 2)
         name = categoryname.args[1]
         category = categoryname.args[2]
+        length(names) == rank(@eval $category) || throw(ArgumentError("Number of names does not match number of objects."))
         name_str = string(name)
         constex = if __module__ == CategoryData
             quote
